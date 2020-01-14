37ce51c89df89b557a96b671b56e9f082b9d73c8
lmehdi <tronligston@gmail.com>
2020-01-14 14:53:03 +0000

    added new Footer by rigui

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,19 +17,11 @@ class Footer extends React.Component {
 								<div className="footer_section footer_about">
 									<div className="footer_logo_container">
 										<a href="#">
-											<div className="footer_logo_text">Unic<span>at</span></div>
+											<div className="footer_logo_text">Gestion des clubs<span></span></div>
 										</a>
 									</div>
 									<div className="footer_about_text">
-										<p>Lorem ipsum dolor sit ametium, consectetur adipiscing elit.</p>
-									</div>
-									<div className="footer_social">
-										<ul>
-											<li><a href="#"><i className="fa fa-facebook" aria-hidden="true"></i></a></li>
-											<li><a href="#"><i className="fa fa-google-plus" aria-hidden="true"></i></a></li>
-											<li><a href="#"><i className="fa fa-instagram" aria-hidden="true"></i></a></li>
-											<li><a href="#"><i className="fa fa-twitter" aria-hidden="true"></i></a></li>
-										</ul>
+										<p></p>
 									</div>
 								</div>
 								
@@ -42,9 +34,11 @@ class Footer extends React.Component {
 									<div className="footer_title">Contact Us</div>
 									<div className="footer_contact_info">
 										<ul>
-											<li>Email: Info.deercreative@gmail.com</li>
-											<li>Phone:  +(88) 111 555 666</li>
-											<li>40 Baria Sreet 133/2 New York City, United States</li>
+											<li>Tél : 0523 40 07 36 </li>
+											<li>Fax : 0523 40 09 69</li>
+											<li>Université Hassan 1er. Faculté des Sciences et Techniques de Settat</li>
+											<li>Email : figi5p@gmail.com</li>
+
 										</ul>
 									</div>
 								</div>
@@ -54,21 +48,7 @@ class Footer extends React.Component {
 							<div className="col-lg-3 footer_col">
 					
 								
-								<div className="footer_section footer_links">
-									<div className="footer_title">Contact Us</div>
-									<div className="footer_links_container">
-										<ul>
-											<li><a href="index.html">Home</a></li>
-											<li><a href="about.html">About</a></li>
-											<li><a href="contact.html">Contact</a></li>
-											<li><a href="#">Features</a></li>
-											<li><a href="courses.html">Courses</a></li>
-											<li><a href="#">Events</a></li>
-											<li><a href="#">Gallery</a></li>
-											<li><a href="#">FAQs</a></li>
-										</ul>
-									</div>
-								</div>
+								
 								
 							</div>
 
@@ -94,15 +74,9 @@ class Footer extends React.Component {
 				<div className="col">
 					<div className="copyright d-flex flex-lg-row flex-column align-items-center justify-content-start">
 						<div className="cr_text">
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i className="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved |  <i className="fa fa-heart-o" aria-hidden="true"></i> <a href="https://colorlib.com" target="_blank">Fillière ingénieur : Génie informatique</a>
 </div>
-						<div className="ml-lg-auto cr_links">
-							<ul className="cr_list">
-								<li><a href="#">Copyright notification</a></li>
-								<li><a href="#">Terms of Use</a></li>
-								<li><a href="#">Privacy Policy</a></li>
-							</ul>
-						</div>
+						
 					</div>
 				</div>
 			</div>
